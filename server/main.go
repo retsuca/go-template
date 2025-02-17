7a43c9f7d9c70e473a00ac027e1b7e5d065165e1
retsuca <ahmetcanugur@gmail.com>
2025-02-17 04:43:21 +0300

    wip

--- server/main.go
+++ server/main.go
@@ -6,6 +6,8 @@ import (
 	"go-template/server/controllers"
 	"net/http"
 
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+
 	"github.com/labstack/echo-contrib/echoprometheus"
 
 	"github.com/labstack/echo/v4"
