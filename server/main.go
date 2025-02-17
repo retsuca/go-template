4018b2603daabff49bfe870a09fdf28c66066824
retsuca <ahmetcanugur@gmail.com>
2025-02-17 04:43:02 +0300

    wip

--- server/main.go
+++ server/main.go
@@ -6,13 +6,7 @@ import (
 	"go-template/server/controllers"
 	"net/http"
 
-	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/propagation"
-
 	"github.com/labstack/echo-contrib/echoprometheus"
-	stdout "go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -21,17 +15,6 @@ import (
 func CreateHTPPServer(host, port string) {
 
 	e := echo.New()
-
-	exporter, err := stdout.New(stdout.WithPrettyPrint())
-	if err != nil {
-		logger.Fatalw("Fatal error initing Tracer ", err)
-	}
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithBatcher(exporter),
-	)
-	otel.SetTracerProvider(tp)
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
